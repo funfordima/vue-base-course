c70978e9e936854dbb040eabb887485aaefd44e5
Dima Litvinov <dlv@vava.cars>
2022-12-28 22:10:40 +0200

    init: create base vue app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="action-block">
+    <button @click="addLike">Like</button>
+    <button v-on:click="addDislike">Dislike</button>
+  </div>
+  <div class="">
+    <p>Likes count: <strong>{{ likes }}</strong></p>
+    <p>Dislikes count: <strong>{{ dislikes }}</strong></p>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
